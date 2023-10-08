30bb5448783142ead0f1b98b2182c0ac1dfbae09
jerichoempleo <tamtamchan12@gmail.com>
2023-10-09 00:58:04 +0800

    Login of 3 users
    nareredirect na ung 3 users sa kanilang specific landing page sa iisang form

--- Client/Login_Submit.php
+++ Client/Login_Submit.php
@@ -1,31 +1,68 @@
 <?php
-    require('../connection.php');
+require('../connection.php');
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
-        // Retrieve the password from the database based on the email
-        $stmt = mysqli_prepare($con, "SELECT client_id, fullname, password FROM client WHERE email = ?");
-        mysqli_stmt_bind_param($stmt, "s", $email);
-        mysqli_stmt_execute($stmt);
-        mysqli_stmt_bind_result($stmt, $client_id, $fullname, $stored_password);
-        mysqli_stmt_fetch($stmt);
+    // Check the client table
+    $stmt = mysqli_prepare($con, "SELECT client_id, fullname FROM client WHERE email = ? AND password = ?");
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $client_id, $fullname);
 
-        if ($password === $stored_password) {
-            // Passwords match, login successful
-            $_SESSION['client_id'] = $client_id; //Here is the Session
-            $_SESSION['fullname'] = $fullname; //Here is the Session | Pagnakaerror dahil to sa fullname na nickname dapat
-            
-
-            header("Location: ../index.php"); // Redirect to client land page | index.php | Pag nasa labas ung file ../ 
-            exit();
-        } else {
-            // Login failed
-            $error_message = "Invalid email or password";
-        }
-
-        // Close the prepared statement
-        mysqli_stmt_close($stmt);
+    if (mysqli_stmt_fetch($stmt)) {
+        // Client login successful
+        $_SESSION['user_id'] = $client_id;
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['role'] = 'client';
+        header("Location: ../index.php"); // Redirect to client dashboard | kapag nasa labas yung file ../ lang
+        exit();
     }
+
+    // Close the statement for client table
+    mysqli_stmt_close($stmt);
+
+    // Check the admin table
+    $stmt = mysqli_prepare($con, "SELECT admin_id, fullname FROM admin WHERE email = ? AND password = ?");
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $admin_id, $fullname);
+
+    if (mysqli_stmt_fetch($stmt)) {
+        // Admin login successful
+        $_SESSION['user_id'] = $admin_id;
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['role'] = 'admin';
+        header("Location: ../Admin/Admin_Dashboard.php"); // Redirect to admin dashboard
+        exit();
+    }
+
+    // Close the statement for admin table
+    mysqli_stmt_close($stmt);
+
+    // Check the mechanic table
+    $stmt = mysqli_prepare($con, "SELECT mechanic_id, fullname FROM mechanic WHERE email = ? AND password = ?");
+    mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_bind_result($stmt, $mechanic_id, $fullname);
+
+    if (mysqli_stmt_fetch($stmt)) {
+        // Mechanic login successful
+        $_SESSION['user_id'] = $mechanic_id;
+        $_SESSION['fullname'] = $fullname;
+        $_SESSION['role'] = 'mechanic';
+        header("Location: ../Mechanic/dashboard.php"); // Redirect to mechanic dashboard
+        exit();
+    }
+
+    // Close the statement for mechanic table
+    mysqli_stmt_close($stmt);
+
+    // If no match found in any table, show an error message
+    $error_message = "Invalid email or password";
+}
+
+// Close the database connection
+mysqli_close($con);
 ?>
