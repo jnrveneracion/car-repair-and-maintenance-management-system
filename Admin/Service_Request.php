328d78a92b97945c20963059508183ff127b9c2a
jnri <jrveneracion13@gmail.com>
2023-10-10 02:55:41 +0800

    view/update service request page done
    sa service history wala pa db retrieval yon

--- Admin/Service_Request.php
+++ Admin/Service_Request.php
@@ -8,43 +8,166 @@ $sql = "SELECT sr.request_id, c.fullname, sr.car_model, sr.service_type
         WHERE sr.request_status = '0'";
 
 $result = mysqli_query($con, $sql);
-
-if (!$result) {
-    echo "Error: " . mysqli_error($con);
-} else {
-    // Check if there are rows in the result set
-    if (mysqli_num_rows($result) > 0) {
-        echo '<h3>Service Request Page</h3>';
-        echo '<table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">Request ID</th>
-                        <th scope="col">Full Name</th>
-                        <th scope="col">Car Model</th>
-                        <th scope="col">Service Type</th>
-                        <th scope="col">Action</th>
-                    </tr>
-                </thead>
-                <tbody>';
-
-        // Loop through the result set and populate the table rows
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo '<tr>
-                    <td>' . $row['request_id'] . '</td>
-                    <td>' . $row['fullname'] . '</td>
-                    <td>' . $row['car_model'] . '</td>
-                    <td>' . $row['service_type'] . '</td>
-                    <td><a href="View_Service_Request.php?request_id=' . $row['request_id'] . '" class="btn btn-primary">View</a></td>
-                </tr>';
-        }
-
-        echo '</tbody></table>';
-    } else {
-        echo "No records found.";
-    }
-
-    mysqli_free_result($result);
-}
-
-mysqli_close($con);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+               integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+               crossorigin="anonymous">
+          <link rel="stylesheet" href="../CSS/style.css">
+          <link rel="stylesheet" href="../CSS/bg-style-a.css">
+          <title>Admin | Service Request</title>
+          <style>
+               .admin-section {
+                    margin-top: 0px;
+                    margin-bottom: 100px;
+                    background-color: rgba(0, 0, 0, 0.29);
+                    min-width: 200px;
+                    overflow: scroll;
+                    padding: 30px;
+                    margin-right: 20px;
+                    margin-left: 20px;
+                    width: 1000px;
+               }
+
+               .table-section {
+                    margin-top: 0px;
+                    min-width: 200px;
+                    overflow: scroll;
+               }
+
+
+               th,
+               td {
+                    text-align: center;
+                    width: fit-content;
+                    padding: 10px 15px;
+               }
+
+               th {
+                    background-color: black!important;
+                    color: white !important;
+                    font-weight: 400;
+                    border: 1px solid white;
+               }
+
+               td {
+                    background-color: transparent !important;
+                    color: white !important;
+                    border: 1px solid white;
+                    vertical-align: middle;
+               }
+
+               .admin-head-txt {
+                    margin: 20px;
+                    color: white;
+                    margin-top: 50px;
+                    text-transform: uppercase;
+                    padding: 10px 35px;
+                    background-color: color(srgb 0.9702 0.7395 0.3452);
+                    width: fit-content;
+               }
+
+               .sec-head {
+                    background-color: color(srgb 0.1294 0.2682 0.2997);
+                    color: white;
+                    padding: 10px;
+                    width: fit-content;
+               }
+
+               .btn-a, .btn-edit, .btn-delete  {
+                    border-radius: 0px;
+                    color: white;
+                    margin: 5px !important;
+                    text-decoration: none;
+                    padding: 10px 20px;
+               }
+
+               .btn-edit {
+                    font-size: 12px;
+                    padding: 5px 10px !important;
+               }
+
+               .btn-a:hover, .btn-edit:hover, .btn-delete:hover  {
+                    filter: brightness(.9);
+               }
+               
+               
+               .btn-a, .btn-edit {
+                    background-color: color(srgb 0.2706 0.7121 0.9729);
+               }
+
+               .btn-a:hover, .btn-edit:hover {
+                    filter: brightness(.9);
+               }
+          </style>
+     </head>
+
+     <body>
+          <?php include "../common/admin-side-nav.php" ?>
+          <div>
+               <div class="row">
+                    <div class="col-xl-2 col-0"></div>
+                    <div class="col-xl-10 col-12">
+                         <div class="d-flex justify-content-center">
+                              <h3 class="admin-head-txt mb-0">Mechanics</h3>
+                         </div>
+                         <div class="d-flex justify-content-center">
+                              <hr style="opacity: 1; color: white; width: 80%;">
+                         </div>
+                         <div class="d-flex justify-content-center">
+                              <div class="admin-section">
+                                   <div class="table-section">
+                                   <?php
+                                   if (!$result) {
+                                        echo "Error: " . mysqli_error($con);
+                                    } else {
+                                        // Check if there are rows in the result set
+                                        if (mysqli_num_rows($result) > 0) {
+                                            echo '<table class="table">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">Request ID</th>
+                                                            <th scope="col">Full Name</th>
+                                                            <th scope="col">Car Model</th>
+                                                            <th scope="col">Service Type</th>
+                                                            <th scope="col">Action</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>';
+                                    
+                                            // Loop through the result set and populate the table rows
+                                            while ($row = mysqli_fetch_assoc($result)) {
+                                                echo '<tr>
+                                                        <td>' . $row['request_id'] . '</td>
+                                                        <td>' . $row['fullname'] . '</td>
+                                                        <td>' . $row['car_model'] . '</td>
+                                                        <td>' . $row['service_type'] . '</td>
+                                                        <td><a href="View_Service_Request.php?request_id=' . $row['request_id'] . '" class="btn-edit">Update</a></td>
+                                                    </tr>';
+                                            }
+                                    
+                                            echo '</tbody></table>';
+                                        } else {
+                                            echo "<div class='text-white'>No records found.</div>";
+                                        }
+                                    
+                                        mysqli_free_result($result);
+                                    }
+                                    
+                                    mysqli_close($con);
+                                   ?>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </body>
+</html>
--- Admin/View_Service_Request.php
+++ Admin/View_Service_Request.php
@@ -9,68 +9,213 @@ if (isset($_GET['request_id'])) {
     $sql = "SELECT * FROM service_request WHERE request_id = $request_id";
     $result = mysqli_query($con, $sql);
 
-    if (!$result) {
-        die("Query failed: " . mysqli_error($con));
-    }
-
-    // Check if there is a row with the specified request ID
-    if (mysqli_num_rows($result) > 0) {
-        echo '<h2>View Service Request</h2>';
-        echo '<h3>Service Request Data</h3>';
-
-        echo '<table class="table">';
-        echo '<thead>';
-        echo '<tr>';
-        echo '<th scope="col">Field Name</th>';
-        echo '<th scope="col">Data</th>';
-        echo '</tr>';
-        echo '</thead>';
-        echo '<tbody>';
-
-        // Fetch and display the data for the specific request ID
-        $row = mysqli_fetch_assoc($result);
-        echo '<tr>';
-        echo '<th scope="row">Request ID</th>';
-        echo '<td>' . $row['request_id'] . '</td>';
-        echo '</tr>';
-
-        echo '<tr>';
-        echo '<th scope="row">Car Model</th>';
-        echo '<td>' . $row['car_model'] . '</td>';
-        echo '</tr>';
-
-        echo '<tr>';
-        echo '<th scope="row">Car Brand</th>';
-        echo '<td>' . $row['car_brand'] . '</td>';
-        echo '</tr>';
-
-        echo '<tr>';
-        echo '<th scope="row">Car Registered Number</th>';
-        echo '<td>' . $row['car_reg_num'] . '</td>';
-        echo '</tr>';
-
-        echo '<tr>';
-        echo '<th scope="row">Service Type</th>';
-        echo '<td>' . $row['service_type'] . '</td>';
-        echo '</tr>';
-
-        echo '<tr>';
-        echo '<th scope="row">Car Problem</th>';
-        echo '<td>' . $row['car_problem'] . '</td>';
-        echo '</tr>';
-
-        echo '</tbody>';
-        echo '</table>';
-
-        echo '<a href="Approve_Service_Request.php?request_id=' . $row['request_id'] . '" class="btn btn-primary">Approve</a>';
-    } else {
-        echo "No records found for the specified request ID.";
-    }
-
-    mysqli_free_result($result);
-} else {
-    echo "Request ID not specified.";
-}
-
-mysqli_close($con);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+     <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+               integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+               crossorigin="anonymous">
+          <link rel="stylesheet" href="../CSS/style.css">
+          <link rel="stylesheet" href="../CSS/bg-style-a.css">
+          <title>Admin | View Service Request</title>
+          <style>
+               .admin-section {
+                    margin-top: 0px;
+                    margin-bottom: 100px;
+                    background-color: rgba(0, 0, 0, 0.29);
+                    min-width: 200px;
+                    overflow: scroll;
+                    padding: 30px;
+                    margin-right: 20px;
+                    margin-left: 20px;
+                    width: 400px;
+               }
+
+               .table-section {
+                    margin-top: 0px;
+                    min-width: 200px;
+                    overflow: scroll;
+               }
+
+
+               th,
+               td {
+                    text-align: center;
+                    width: fit-content;
+                    padding: 10px 15px;
+               }
+
+               th {
+                    background-color: black!important;
+                    color: white !important;
+                    font-weight: 400;
+                    border: 1px solid white;
+               }
+
+               td {
+                    background-color: transparent !important;
+                    color: white !important;
+                    border: 1px solid white;
+                    vertical-align: middle;
+               }
+
+               .admin-head-txt {
+                    margin: 20px;
+                    color: white;
+                    margin-top: 50px;
+                    text-transform: uppercase;
+                    padding: 10px 35px;
+                    background-color: color(srgb 0.9702 0.7395 0.3452);
+                    width: fit-content;
+               }
+
+               .sec-head {
+                    background-color: color(srgb 0.1294 0.2682 0.2997);
+                    color: white;
+                    padding: 10px;
+                    width: fit-content;
+               }
+
+               .btn-a, .btn-edit, .btn-delete  {
+                    border-radius: 0px;
+                    color: white;
+                    margin: 5px !important;
+                    text-decoration: none;
+                    padding: 10px 20px;
+               }
+
+               .btn-edit {
+                    font-size: 12px;
+                    padding: 5px 10px !important;
+               }
+
+               .btn-a:hover, .btn-edit:hover, .btn-delete:hover  {
+                    filter: brightness(.9);
+               }
+               
+               
+               .btn-a, .btn-edit {
+                    background-color: color(srgb 0.2706 0.7121 0.9729);
+               }
+
+               .btn-a:hover, .btn-edit:hover, .btn-b:hover {
+                    filter: brightness(.9);
+               }
+
+               .buttons {
+                    padding: 5px 20px;
+               }
+
+               .btn-b.buttons {
+                    background-color: red;
+                    text-decoration: none;
+                    color: black;
+                    padding: 8px 20px;
+               }
+
+               .buttons:hover {
+                    filter: brightness(.9)
+               }
+
+               .Active {
+                    color: rgb(0, 208, 0) !important;
+               }
+
+               .Inactive {
+                    color: red !important;
+               }
+          </style>
+     </head>
+
+     <body>
+          <?php include "../common/admin-side-nav.php" ?>
+          <div>
+               <div class="row">
+                    <div class="col-xl-2 col-0"></div>
+                    <div class="col-xl-10 col-12">
+                         <div class="d-flex justify-content-center">
+                              <h3 class="admin-head-txt mb-0">Update Request Status</h3>
+                         </div>
+                         <div class="d-flex justify-content-center">
+                              <hr style="opacity: 1; color: white; width: 80%;">
+                         </div>
+                         <div class="d-flex justify-content-center">
+                              <div class="admin-section">
+                                   <div class="table-section">
+                                   <?php
+                                   if (!$result) {
+                                        die("Query failed: " . mysqli_error($con));
+                                        }
+                                   
+                                        // Check if there is a row with the specified request ID
+                                        if (mysqli_num_rows($result) > 0) {
+                                             echo '<table class="table">';
+                                             echo '<thead>';
+                                             echo '<tr>';
+                                             echo '<th scope="col">Field Name</th>';
+                                             echo '<th scope="col">Data</th>';
+                                             echo '</tr>';
+                                             echo '</thead>';
+                                             echo '<tbody>';
+                                   
+                                             // Fetch and display the data for the specific request ID
+                                             $row = mysqli_fetch_assoc($result);
+                                             echo '<tr>';
+                                             echo '<th scope="row">Request ID</th>';
+                                             echo '<td>' . $row['request_id'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '<tr>';
+                                             echo '<th scope="row">Car Model</th>';
+                                             echo '<td>' . $row['car_model'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '<tr>';
+                                             echo '<th scope="row">Car Brand</th>';
+                                             echo '<td>' . $row['car_brand'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '<tr>';
+                                             echo '<th scope="row">Car Registered Number</th>';
+                                             echo '<td>' . $row['car_reg_num'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '<tr>';
+                                             echo '<th scope="row">Service Type</th>';
+                                             echo '<td>' . $row['service_type'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '<tr>';
+                                             echo '<th scope="row">Car Problem</th>';
+                                             echo '<td>' . $row['car_problem'] . '</td>';
+                                             echo '</tr>';
+                                   
+                                             echo '</tbody>';
+                                             echo '</table>';
+                                   
+                                             echo '<a href="Approve_Service_Request.php?request_id=' . $row['request_id'] . '" class="btn-a buttons">Approve</a>';
+                                             echo '<a href="Service_Request.php" class="btn-b buttons">Cancel</a>';
+                                        } else {
+                                             echo "<div class='text-white'>No records found for the specified request ID.</div>";
+                                        }
+                                   
+                                        mysqli_free_result($result);
+                                   } else {
+                                        echo "Request ID not specified.";
+                                   }
+                                   
+                                   mysqli_close($con);
+                                   ?>
+                                   </div>
+                              </div>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </body>
+</html>
